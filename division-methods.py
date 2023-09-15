2668e7ea75e8dc6d1df242fa352a030a3911577f
Jeff Jacobsen <jeff.jacobsen.uw@gmail.com>
2023-09-15 16:54:42 -0700

    perhaps ready to submit

--- division-methods.py
+++ division-methods.py
@@ -3,5 +3,9 @@ if __name__ == '__main__':
     b = int(input())
 
     # check constraints
-    
+    # to avoid division by zero error, b cannot = 0
+
     # perform division operations
+    if(b != 0):
+        print( a // b )
+        print( a / b )
