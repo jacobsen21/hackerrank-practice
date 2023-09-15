b34006ad9aa75ddeeb285ec02a8eca5d6873c267
Jeff Jacobsen <jeff.jacobsen.uw@gmail.com>
2023-09-15 11:41:52 -0700

    adjusted exponential input allowance and syntax

--- arith-methods-practice.py
+++ arith-methods-practice.py
@@ -1,21 +1,25 @@
 #!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
-import pytest
-
-
 if __name__ == '__main__':
 
     a = int(input())
     b = int(input())
 
     # check for valid input range for a and b
-    if(1 <= a <= 100) and (1 <= b <= 100):
+    if(1 <= a <= 10**10) and (1 <= b <= 10**10):
         # do operations - print output
         print(a + b)
         print(a - b)
         print(a * b)
+
+    # 6564424525
+    # 323252462
+    # ckerrank-practice/arith-methods-practice.py
+    # 6564424525
+    # 323252462
+    # 6887676987
+    # 6241172063
+    # 2121966389319430550
+
+    # this was about getting the allowable exponential inputs to
+    # be allowed. the large input was from the hidden test case.
