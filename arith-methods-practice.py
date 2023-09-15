214e711a964a6d8bf634e36797a711df9a94e53b
Jeff Jacobsen <jeff.jacobsen.uw@gmail.com>
2023-09-15 11:05:48 -0700

    revise problem solution

--- arith-methods-practice.py
+++ arith-methods-practice.py
@@ -10,22 +10,12 @@ import pytest
 
 if __name__ == '__main__':
 
-    a = 0
-    b = 0
-    
-    while not (1 <= a <= 100):
-        a = int(input())
-
-    while not (1 <= b <= 100):
-        b = int(input())
+    a = int(input())
+    b = int(input())
 
     # check for valid input range for a and b
-    # if(1 <= a <= 100 and 1 <= b <= 100):
-
-    # do operations - print output
-    print(a + b)
-    print(a - b)
-    print(a * b)
-
-    #else:
-        # print("invalid input")
+    if(1 <= a <= 100) and (1 <= b <= 100):
+        # do operations - print output
+        print(a + b)
+        print(a - b)
+        print(a * b)
