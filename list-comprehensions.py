063d6e2f73b0d0976038bcae2a619ecaa4a5a98d
Jeff Jacobsen <jeff.jacobsen.uw@gmail.com>
2023-10-04 15:36:34 -0700

    submitted and passed

--- list-comprehensions.py
+++ list-comprehensions.py
@@ -11,10 +11,15 @@ if __name__ == '__main__':
     # print(newlist)
 
     # generate initial list of permutations from x,y,z
-    iters = [[i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1)]
+    iters = [[i, j, k] for i in range(x+1) \
+             for j in range(y+1) \
+             for k in range(z+1)]
     print(iters)
 
     # create comprehensive new list that only includes permutations != 3
-    newiters =  [[i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n]
+    newiters =  [[i, j, k] for i in range(x+1) \
+        for j in range(y+1) \
+        for k in range(z+1) \
+        if i+j+k != n]
     print(newiters)
     
