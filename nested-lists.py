cf5803e26589cec3feae7642423dfa98fb54cd3f
Jeff Jacobsen <jeff.jacobsen.uw@gmail.com>
2023-10-07 22:05:46 -0700

    finally got it working. just need to remove scaffolding for correct output and submit

--- nested-lists.py
+++ nested-lists.py
@@ -8,42 +8,53 @@ if __name__ == '__main__':
 
     #     records.append([name, score])
 
-    # sort by name function
-    def sortbyvalue(records):
-        return(sorted(records, key=lambda x: x[1], reverse=False))
-        
     # debug 
-    n = 3
-    records = [['first', 53.21], ['third', 3.33], ['second', 232.22]]
-    print(records)
-
+    n = 4
+    records = [['first', 3.33], ['breaker19', 3.33], ['third', 3.33], ['second', 232.22]]
+   
+    # print(records)
     # min/max
     # print("max: ", max(records))
     # print("min: ", min(records))
-    # print("first in list: ", records[0])
-
-    # initial alpha sort
+   
+    # initial alpha sort to get names in alpha order
     records.sort()
+
+    print("initial alpha sort on names")
     print(records)
 
-    # newlist = sort(records[1])
-    # print(newlist)
-
-    # sortbyvalue(records)
-    # print(records)
-
-
-    # sort / reverse 
-    # print("reverse sorted: ", records.sort(reverse=True))
-    # print("second to worst score: ", records[n-2])
+    # sort by scores
+    records.sort(key=lambda x : x[1]) # x[element 0 or 1]
     
-    # nested for loop to access each name/value pair
-    # for student, score in records: 
-    #     print(student)
-    #     for score in student:
-    #         print(score)
+    print("sorted low to hi scores: ") 
+    print(records)
 
-    # determine how to sort values 
-    # then select idx 1 and check if idx1 + 1 is equal 
-    # do not exceed n entries
+    print("lowest score: ")
+    print(records[0])
+
+    print("2nd lowest score: ")
+    print(records[1])
+
+    # set base name and score values from second lowest score
+    secname, secscore = records[1]
+
+    # print("sec name: ")
+    print(secname)
+    
+    # set counters for duplicate search
+    m = 2
+
+    # compare for duplicate second lowest scores
+    while m < n:
+
+        # set individual name and score elements from current record
+        name, score = records[m]     
+
+        # if score is equal to seclow base then print name 
+        if(score == secscore):
+            print(name)
+            #break
+            # keep looking, just don't print matching dupe names
+        m = m + 1
+
     
