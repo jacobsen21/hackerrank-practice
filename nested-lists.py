a450488f5e35be4096d0a5032171b8f74bfaffbf
Jeff Jacobsen <jeff.jacobsen.uw@gmail.com>
2023-10-09 12:54:58 -0700

    save here. fix 2 failing hidden TCs

--- nested-lists.py
+++ nested-lists.py
@@ -8,43 +8,28 @@ if __name__ == '__main__':
 
     #     records.append([name, score])
 
-    # debug 
-    n = 4
-    records = [['first', 3.33], ['breaker19', 3.33], ['third', 3.33], ['second', 232.22]]
+    # n = len(records)
+
+    # debug - hidden test case #2
+    n = 5
+    records = [['Harsh', 20], ['Beria', 20], ['Varum', 19], 
+               ['Kakunami', 19], ['Vikas',21]]
+    # expected output: Beria, Harsh
    
-    # print(records)
-    # min/max
-    # print("max: ", max(records))
-    # print("min: ", min(records))
-   
+
     # initial alpha sort to get names in alpha order
     records.sort()
 
-    print("initial alpha sort on names")
-    print(records)
-
-    # sort by scores
-    records.sort(key=lambda x : x[1]) # x[element 0 or 1]
+    # sort by scores - x[element 0 or 1]
+    records.sort(key=lambda x : x[1])
     
-    print("sorted low to hi scores: ") 
-    print(records)
-
-    print("lowest score: ")
-    print(records[0])
-
-    print("2nd lowest score: ")
-    print(records[1])
-
     # set base name and score values from second lowest score
     secname, secscore = records[1]
-
-    # print("sec name: ")
     print(secname)
     
+    # compare for duplicate second lowest scores
     # set counters for duplicate search
     m = 2
-
-    # compare for duplicate second lowest scores
     while m < n:
 
         # set individual name and score elements from current record
