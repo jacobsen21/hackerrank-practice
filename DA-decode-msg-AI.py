46409e31f5ff0f801f2547802276ec9a60529989
Jeff Jacobsen <83145999+jacobsen21@users.noreply.github.com>
2023-11-01 11:27:19 -0700

    init commit for DA challenge files on remote GITHUB

---
+++ DA-decode-msg-AI.py
@@ -0,0 +1,40 @@
+'''
+I need a function called decode(message_file) that can read in an encoded message from a .txt file and return the decoded version as a string.
+
+Here's an example of what the message_file .txt file will look like:
+
+3 love
+6 computers
+2 dogs
+4 cats
+1 I
+5 you
+
+As you can see, each word is associated with a number. Imagine you ordered all those numbers from smallest to biggest and arranged them into a pyramid. Each line of the pyramid includes one more number than the line before it:
+
+1
+2 3
+4 5 6
+
+The numbers at the end of each line (1, 3 and 6) correspond to the words that are part of the message. You should ignore all the other words. So for the example input file above, the message words are:
+
+1: I
+3: love
+6: computers
+
+and your function should return the string "I love computers"
+'''
+# path to message_file.txt
+message_file = '.'
+
+# read the file
+try:
+    with open(message_file, 'r') as file:
+        lines = file.readlines()
+
+except FileNotFoundError:
+    raise FileNotFoundError(f"the file: '{message_file} not found")
+
+# decode function
+def decode(message_file):
+    pass
